90a5fd192a24de3b64f53d0ab65e9b419484b97f
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-02 10:55:24 -0700

    Initial Project

---
+++ WeatherSwiftUI/WeatherSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherSwiftUI
+//
+//  Created by projas on 12/2/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
