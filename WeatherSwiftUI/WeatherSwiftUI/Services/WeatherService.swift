29a0a80b1947d5f1946874af1b562c3a471939e1
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-02 13:08:26 -0700

    Fix issues with url format and status code

--- WeatherSwiftUI/WeatherSwiftUI/Services/WeatherService.swift
+++ WeatherSwiftUI/WeatherSwiftUI/Services/WeatherService.swift
@@ -16,14 +16,16 @@ class WeatherService {
   private let apiKey = "08f1026c76f4309cf4754009e10eac06"
   
   func getWeather(city: String, completion: @escaping (Weather?) -> Void) {
-    let stringUrl = "\(baseURL)/\(versionAPI)/\(typeAPI)?city=\(city)&appid=\(apiKey)&units=metric"
+    let stringUrl = "\(baseURL)/\(versionAPI)/\(typeAPI)?q=\(city)&appid=\(apiKey)&units=metric"
     guard let url = URL(string: stringUrl) else {
       completion(nil)
       return
     }
     
     URLSession.shared.dataTask(with: url) { data, response, error in
-      guard let data = data, error == nil else {
+
+      guard let data = data, error == nil,
+        let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
         completion(nil)
         return
       }
