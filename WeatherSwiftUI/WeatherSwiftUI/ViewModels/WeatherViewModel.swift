5495b58a30586be373779e67b5bf38ab52f8af85
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-02 11:55:46 -0700

    Configure ViewModel

---
+++ WeatherSwiftUI/WeatherSwiftUI/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherViewModel.swift
+//  WeatherSwiftUI
+//
+//  Created by projas on 12/2/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class WeatherViewModel: ObservableObject {
+  private var service: WeatherService!
+  @Published var weather: Weather?
+  
+  init() {
+    self.service = WeatherService()
+  }
+  
+  var temperature: String {
+    if let temp = self.weather?.temperature {
+      return String(format: "%.0f", temp)
+    }
+    
+    return ""
+  }
+  
+  var humidity: String {
+    if let humidity = self.weather?.humidity {
+      return String(format: "%.0f", humidity)
+    }
+    
+    return ""
+  }
+  
+  var cityName = ""
+  
+  func search() {
+    if let city = self.cityName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+      fetchWeather(by: city)
+    }
+  }
+  
+  private func fetchWeather(by city: String) {
+    self.service.getWeather(city: city) { weather in
+      self.weather = weather
+    }
+  }
+}
